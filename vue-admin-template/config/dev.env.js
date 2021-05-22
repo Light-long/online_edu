b1c48acd6e751438db41ce54ed5eeed9921cbad6
Shilong Li <lishilong1108@foxmail.com>
2021-05-22 15:50:31 +0800

    使用nginx转发请求

--- vue-admin-template/config/dev.env.js
+++ vue-admin-template/config/dev.env.js
@@ -6,5 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
   // 这个是‘ “ ”’，单引号+双引号
-  BASE_API: '"http://localhost:8001"',
+  // 将请求IP和端口改为nginx的9001
+  BASE_API: '"http://localhost:9001"',
 })
